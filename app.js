10e18dd5a5d05ef6ff1bc41dba9a656df2aaae63
Aaqib-Ahmed-Khan <155422396+Aaqib-Ahmed-Khan@users.noreply.github.com>
2024-06-06 11:49:53 +0500

    login log out working

--- app.js
+++ app.js
@@ -2,20 +2,27 @@ var email=document.getElementById('email');
 var password=document.getElementById('password');
 var login_container=document.getElementById("login_container");
 var home_container=document.getElementById("home_container");
-function loginuser(){
+var user_email=document.getElementById("user_email");
+function loginUser(){
     if(!email.value ||!password.value)
         return alert ("Please enter your email or password");
 localStorage.setItem("email",email.value);
+checkIsUserLoggedin()
  }
-function checkisuserlogin(){
-    var email=localstorage.getitem("email")
+function checkIsUserLoggedin(){
+    var email=localStorage.getItem("email")
     if(email){
         login_container.style.display="none";
         home_container.style.display="block";
+        user_email.innerText=email;
     }
     else{
         login_container.style.display="block";
         home_container.style.display="none";
  }
 }
-checkisuserlogin();
+checkIsUserLoggedin();
+function logout(){
+    localStorage.removeItem("email");
+    checkIsUserLoggedin()
+}
