93afec4277ed9c77c0b5846edc01b1ce114f37c8
Aaqib-Ahmed-Khan <155422396+Aaqib-Ahmed-Khan@users.noreply.github.com>
2024-06-06 10:27:20 +0500

    local storage working

--- app.js
+++ app.js
@@ -1,2 +1,21 @@
-var email=document.getElementById('email')
-var password=document.getElementById('password')
+var email=document.getElementById('email');
+var password=document.getElementById('password');
+var login_container=document.getElementById("login_container");
+var home_container=document.getElementById("home_container");
+function loginuser(){
+    if(!email.value ||!password.value)
+        return alert ("Please enter your email or password");
+localStorage.setItem("email",email.value);
+ }
+function checkuserlogin(){
+    var email=localstorage.getitem('email')
+    if(email){
+        login_container.style.display='none';
+        home_container.style.display='block';
+    }
+    else{
+        login_container.style.display='block';
+        home_container.style.display='none';
+ }
+}
+checkIsUserLogin();
